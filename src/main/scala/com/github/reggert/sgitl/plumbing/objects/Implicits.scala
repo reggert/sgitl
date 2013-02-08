8f882a631a09b81890d9823d9b6e385ae66046ac
Richard W. Eggert II <richard.eggert@gmail.com>
2013-02-07 21:04:00 -0500

    streamlined IteratorInputStream

--- src/main/scala/com/github/reggert/sgitl/plumbing/objects/Implicits.scala
+++ src/main/scala/com/github/reggert/sgitl/plumbing/objects/Implicits.scala
@@ -14,13 +14,9 @@ trait Implicits
 	implicit def inputStream2Iterator(inputStream : InputStream) : Iterator[Byte] =
 		Iterator.continually(inputStream.read()).takeWhile(_ != -1).map(_.toByte)
 	
-	implicit class IteratorInputStream(val iterator : Iterator[Byte]) extends InputStream
+	implicit final class IteratorInputStream(val iterator : Iterator[Byte]) extends InputStream
 	{
-		override def read() =
-			if (!iterator.hasNext)
-				-1
-			else
-				iterator.next
+		override def read() = if (iterator.hasNext) iterator.next else -1
 	}
 
 	implicit def iterable2InputStream(iterable : Iterable[Byte]) =
